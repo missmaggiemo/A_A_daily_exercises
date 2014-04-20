6193240cd91295a8d6dfef71b2ee51c64063b88c
missmaggiemo <margaret.m.moreno@gmail.com>
2014-04-20 13:39:11 -0700

    cleaning up merge sort

--- W1/W1D4/lib/merge_sort.rb
+++ W1/W1D4/lib/merge_sort.rb
@@ -3,31 +3,11 @@ def merge_sort(array)
     return array
   end
 
-  # if array.length == 2
-  #   case array[0] <=> array[1]
-  #     when -1
-  #       return array
-  #     when 0
-  #       return array
-  #     when 1
-  #       return array.reverse
-  #     end
-  #   end
-
   mid = array.length/2
   left = array[0..mid]
   right = array[mid+1..-1]
   merge(merge_sort(left), merge_sort(right))
-  #
-  # sorted_left =  merge_sort(left)
-  # sorted_right = merge_sort(right)
-  #
-  # [sorted_left] + [sorted_right]
-  #
-  # p "Sorted left = #{sorted_left}"
-  # p "Sorted right = #{sorted_right}"
-  # p array
-  #
+
 end
 
 def merge(arr1, arr2)
